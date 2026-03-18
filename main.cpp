5825c9a9f50971f55f531b89d508a51c0336f424
FShaaM <zinovevas135@gmail.com>
2026-03-18 18:02:45 +0300

    Added function

--- main.cpp
+++ main.cpp
@@ -13,5 +13,67 @@ struct Vec
 	size_t cap;
 };
 
+template< class T >
+Vec< List< T >* > balanced_move(Vec< List< T >* > v, size_t k)
+{
+	size_t total = 0;
+	for (size_t i = 0; i < v.s; ++i)
+	{
+		List<T>* node = v.data[i];
+		while (node)
+		{
+			++total;
+			node = node->next;
+		}
+	}
+
+	size_t col = (total + k - 1) / k;
+
+	List< T >** heads = nullptr;
+	List< T >** tails = nullptr;
+
+	List< T >** heads = new List< T >[col];
+	List< T >** tails = new List< T >[col];
+
+	for (size_t i = 0; i < new_count; ++i)
+	  heads[i] = tails[i] = nullptr;
+
+	size_t processed = 0;
+	for (size_t i = 0; i < v.s; ++i)
+	{
+		List<T>* node = v.data[i];
+		v.data[i] = nullptr;
+
+		while (node)
+		{
+			List<T>* next = node->next;
+			node->next = nullptr;
+
+			size_t idx = processed / k;
+
+			if (heads[idx] == nullptr)
+			{
+				heads[idx] = tails[idx] = node;
+			}
+			else
+			{
+				tails[idx]->next = node;
+				tails[idx] = node;
+			}
+			++processed;
+			node = next;
+		}
+	}
+
+	Vec<List<T>*> result;
+	result.data = heads;
+	result.s = col;
+	result.cap = col;
+
+	delete[] tails;
+
+	return result;
+}
+
 int main()
 {}
